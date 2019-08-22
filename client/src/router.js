152534b3af86aeb63bea4aa5310312040abd6630
unknown <1456421562@qq.com>
2019-08-22 13:28:46 +0800

    路由守卫问题

--- client/src/router.js
+++ client/src/router.js
@@ -65,7 +65,7 @@ var router = new Router({
 // 路由守卫
 router.beforeEach((to, from, next) => {
   const isLogin = localStorage.getItem('eletoken')? true: false
-  if(to.path === '/login' || to.path ==='register'){
+  if(to.path === '/login' || to.path ==='/register'){
     next()
   }else{
     isLogin?next():next('/login')
